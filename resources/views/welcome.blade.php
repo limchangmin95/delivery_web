87365928698e1ae23146bb7d7ad0799f088b74d0
limchangmin95 <mnxk90@naver.com>
2022-08-27 15:22:28 +0900

    Initial Setting

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,25 @@
+<head>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <!-- <script src="{{ asset('js/app.js') }}" defer></script> -->
+    <script src="{{ mix('js/app.js') }}"></script>
+    <!-- common.js 추가 -->
+    <script src="{{ mix('js/common.js') }}"></script>
+</head>
+
+<div class="mb-3">
+  <label for="exampleFormControlInput1" class="form-label">Email address</label>
+  <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+</div>
+<div class="mb-3">
+    <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
+    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+    <button type="button" class="btn btn-primary">Primary</button>
+    <button type="button" class="btn btn-secondary">Secondary</button>
+    <button type="button" class="btn btn-success">Success</button>
+    <button type="button" class="btn btn-danger">Danger</button>
+    <button type="button" class="btn btn-warning">Warning</button>
+    <button type="button" class="btn btn-info">Info</button>
+    <button type="button" class="btn btn-light">Light</button>
+    <button type="button" class="btn btn-dark">Dark</button>
+    <button type="button" class="btn btn-link">Link</button>
+</div>
